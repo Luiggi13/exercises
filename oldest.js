119d7f425a715f15cc582add2be376c93ec63b04
Christian Llansola Llopis <christian.llansola@mail.schwarz>
2023-11-03 20:57:43 +0100

    oldest

--- oldest.js
+++ oldest.js
@@ -1,25 +1,12 @@
-function encontrarPersonaMayor(peopleArray) {
-    let personaMayor = null;
-    let edadMayor = 0;
+  function findTheOldest(people) {
     const currentYear = new Date().getFullYear();
-  
-    for (let i = 0; i < peopleArray.length; i++) {
-      const persona = peopleArray[i];
-      let edad;
-  
-      if (persona.dead) {
-        // Si la persona está fallecida, calcula su edad restando el año de nacimiento al año de fallecimiento
-        edad = persona.dead - persona.born;
-      } else {
-        // Si la persona aún está viva, calcula su edad restando el año de nacimiento al año actual
-        edad = currentYear - persona.born;
-      }
-  
-      if (edad > edadMayor) {
-        edadMayor = edad;
-        personaMayor = persona;
-      }
-    }
-  
-    return personaMayor;
+    const oldestPerson = people.reduce((oldest, person) => {
+      const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+      const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+      return age > oldestAge ? person : oldest;
+    });
+    return oldestPerson;
   }
+  
+  module.exports = findTheOldest;
+  
